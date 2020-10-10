9fb43af52e883fed869a677322908aa91ec89362
akshat srivastava <64839659+Jrkej@users.noreply.github.com>
2020-10-10 13:42:26 +0530

    lower() added

--- setup.py
+++ setup.py
@@ -263,7 +263,7 @@ def scrape_data(part):
         update_first()
     for cgdata in leaderboard:
         try:
-            if cgdata['pseudo'] in participants:
+            if cgdata['pseudo'] in participants or cgdata['pseudo'].lower() in participants:
                 username = cgdata['pseudo']
                 cgrank = cgdata['rank']
                 lerank = cgdata['localRank']
